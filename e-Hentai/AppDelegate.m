6e999c67d12a0bcb73b7402ec2938b469260b7b4
28elver <28elver@gmail.com>
2014-09-30 11:21:35 +0800

    Appdelegate 修正判斷

--- e-Hentai/AppDelegate.m
+++ e-Hentai/AppDelegate.m
@@ -11,20 +11,22 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-	self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-	//區分iPad
-	HentaiNavigationController *hentaiNavigation;
-	if ([[UIDevice currentDevice].model isEqualToString:@"iPad"]) {
-		hentaiNavigation = [[HentaiNavigationController alloc] initWithRootViewController:[Pad_Main_VCLR new]];
-	}
-	else {
-		hentaiNavigation = [[HentaiNavigationController alloc] initWithRootViewController:[MainViewController new]];
-	}
-	hentaiNavigation.autorotate = NO;
-	hentaiNavigation.hentaiMask = UIInterfaceOrientationMaskPortrait;
-	self.window.rootViewController = hentaiNavigation;
-	[self.window makeKeyAndVisible];
-	return YES;
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    //區分iPad
+    NSString *zStrDeviceMode = [UIDevice currentDevice].model;
+    HentaiNavigationController *hentaiNavigation;
+    if ([zStrDeviceMode isEqualToString:@"iPad"]
+        || [zStrDeviceMode isEqualToString:@"iPad Simulator"]) {
+        hentaiNavigation = [[HentaiNavigationController alloc] initWithRootViewController:[Pad_Main_VCLR new]];
+    }
+    else {
+        hentaiNavigation = [[HentaiNavigationController alloc] initWithRootViewController:[MainViewController new]];
+    }
+    hentaiNavigation.autorotate = NO;
+    hentaiNavigation.hentaiMask = UIInterfaceOrientationMaskPortrait;
+    self.window.rootViewController = hentaiNavigation;
+    [self.window makeKeyAndVisible];
+    return YES;
 }
 
 @end
